a10aa6b1034c38692fd39f3865e8a6dc4d62fbfb
dtchin2 <dtchin2@gmail.com>
2023-12-15 10:50:45 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
 
 function App() {
+  const [amountConverted, setAmountConverted] = useState('Exchange Rate');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App background">
+      <h2>Currency Converter</h2>
+      <h3>{amountConverted}</h3>
+      <br/>
+      <input type='number' id='convertAmountInput' className='convert_amount_input' placeholder='Enter Amount to Convert' min={'0'}/>
+      <br/>
+      <label>From</label>
+      <select id='fromUnit' className='to_from_selects'>
+        <option value={'USD'}>USD</option>
+        <option value={'EURO'}>EURO</option>
+      </select>
+      <label>To</label>
+      <select id='toUnit' className='to_from_selects'>
+        <option value={'USD'}>USD</option>
+        <option value={'EURO'}>EURO</option>
+      </select>
+      <br/>
+      <Button className='convert_button' onClick={() => setAmountConverted(getConvertedAmount)}>Convert</Button>
     </div>
   );
+}
+
+function getConvertedAmount(){
+  var usdtoEuroExchangeRate = 0.90918882
+  var eurotoUSDExchangeRate = 1.0995797
+  var amountToConvert = document.getElementById('convertAmountInput').value;
+
+  var fromUnit = document.getElementById('fromUnit').value
+  var toUnit = document.getElementById('toUnit').value;
+
+  if(fromUnit === toUnit){
+    alert("Conversion Values Cannot be same.")
+    return 'Enter Amount to Convert';
+  }
+  
+  var exchangeRate = 0.0;
+  if(fromUnit === 'USD'){
+    exchangeRate = usdtoEuroExchangeRate;
+  }else if(fromUnit === 'EURO'){
+    exchangeRate = eurotoUSDExchangeRate;
+  }
+
+  var converedAmount = amountToConvert * exchangeRate;
+
+  return converedAmount + " " + toUnit;
 }
 
 export default App;
